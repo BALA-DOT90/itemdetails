1094f6b6ba6f2330cea633453174795b676fcfd5
BALA-DOT90 <83537299+BALA-DOT90@users.noreply.github.com>
2021-05-06 08:23:18 +0530

    Add files via upload

---
+++ src/main/java/com/item/details/exception/ItemException.java
@@ -0,0 +1,11 @@
+package com.item.details.exception;
+
+public class ItemException extends Exception{
+	
+	private static final long serialVersionUID = -3859492086970470403L;
+
+	public ItemException(String message) {
+		super(message);
+	}
+
+}
