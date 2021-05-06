1a6abfd1365e0189129a399b43a1989bfd9b8a9f
BALA-DOT90 <83537299+BALA-DOT90@users.noreply.github.com>
2021-05-06 08:28:04 +0530

    Add files via upload

---
+++ src/main/java/com/item/details/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.item.details.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.item.details.model.Items;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Items, Integer>{
+
+
+}
